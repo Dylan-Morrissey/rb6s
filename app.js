61498a00f673a626a5b08e030bf22db957ade97f
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2018-10-09 00:16:38 +0100

    Added a modeles for operators, added a few https methods (Gets, Post, Delete, Put)

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const operators = require("./routes/operators");
 
 var app = express();
 
@@ -21,6 +22,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/operators', operators.findAll);
+app.get('/operators/:name', operators.findOne);
+
+app.post('/operators', operators.addOperator);
+
+app.put('/operators/:name/vote', operators.incrementUpvotes);
+
+app.delete('/operators:/name', operators.deleteOperator);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/operators.js
@@ -0,0 +1,73 @@
+let operators = require('../models/operators');
+let express = require('express');
+let router = express.Router();
+
+router.findAll = (req, res) => {
+    // Return a JSON representation of our list
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(donations,null,5));
+}
+
+router.findOne = (req, res) => {
+
+    res.setHeader('Content-Type', 'application/json');
+    var operator = getByValue(operators, req.params.name)
+
+    if (operator != null)
+        res.send(JSON.stringify(operator, null, 5));
+    else
+        res.send('Operator Not Found');
+}
+
+router.addOperator = (req, res) => {
+    var id = Math.floor((Math.random() * 1000000) + 1); //Randomly generate an id
+    // parameters to store
+    // id (for id)
+    // req.body.paymenttype (for paymenttype)
+    // req.body.amount (for amount)
+    // 0 (for upvotes)
+    var currentSize = operators.length;
+
+    operators.push({id: id, name: req.body.name, side: req.body.side, force: req.body.force, gadget: req.body.gadget, upvotes: 0});
+
+    if((currentSize + 1) == operators.length)
+        res.json({ message: 'Operator Added!'});
+    else
+        res.json({ message: 'Operator NOT Added!'});
+}
+
+router.incrementUpvotes = (req, res) => {
+    // Find the relevant donation based on params id passed in
+    // Add 1 to upvotes property of the selected donation based on its id
+    var operator = getByValue(operators,req.params.name);
+
+    if (operator!=null) {
+        operators.upvotes += 1;
+        res.json({status: 200, message: 'Up Vote Successful', operator: operator});
+    } else
+        res.send('Operator Not found -up vote not successful');
+}
+
+router.deleteOperator =(req,res)=> {
+
+    var operator = getByValue(operators, req.params.name);
+    var index = operators.indexOf(operator)
+
+    var currentSize = operators.length
+    if (operator != null) {
+        if ((currentSize - 1) == operators.length)
+            res.json({message: 'Operator Deleted'});
+        else
+            res.json({message: 'Operator not deleted'});
+        res.send(JSON.stringify(operator, null, 5));
+    }
+    else
+        res.send('Operator Not Found');
+}
+
+function getByValue(array, id) {
+    var result  = array.filter(function(obj){return obj.id == id;} );
+    return result ? result[0] : null; // or undefined
+}
+
+module.exports = router
